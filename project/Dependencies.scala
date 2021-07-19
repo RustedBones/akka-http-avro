04332f67439c71c6f3b21905c05f259bc1c7af05
Scala Steward <me@scala-steward.org>
2021-07-19 13:25:23 +0200

    Update logback-classic to 1.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val akkaHttp  = "10.2.4"
     val avro      = "1.10.2"
     val avro4s    = "3.0.8"
-    val logback   = "1.2.3"
+    val logback   = "1.2.4"
     val scalaTest = "3.2.8"
   }
 
