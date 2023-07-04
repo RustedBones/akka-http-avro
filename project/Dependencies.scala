cf27799e7b523ba39d9dcc7f13a2f15a6e6b8df1
Scala Steward <scala_steward@virtuslab.com>
2023-07-04 20:48:03 +0000

    Update avro, avro-compiler to 1.11.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val akka      = "2.6.19"
     val akkaHttp  = "10.2.9"
-    val avro      = "1.11.0"
+    val avro      = "1.11.2"
     val avro4s    = "3.0.8"
     val logback   = "1.2.11"
     val scalaTest = "3.2.12"
